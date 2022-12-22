ec120028668ddbf7dc8b1753ad0227742c4515a1
Stu88S <stu88s.tor@gmail.com>
2022-12-22 15:04:23 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const KEY = '70641116-f90b-41d3-9e3a-7eb78d9bc0d5';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
